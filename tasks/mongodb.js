6e20449894a87d0bc866bc59e87e116516d77a23
Travis McHattie <travis.mchattie@thinksolid.com>
2014-02-16 14:19:25 -0800

    Fixed linting error.

--- tasks/mongodb.js
+++ tasks/mongodb.js
@@ -75,7 +75,10 @@ module.exports = function (grunt) {
 
     function cleanCollection(col, callback){
         client.connect(host, function(err, db) {
-            if (err) grunt.log.errorlns(err);
+            if (err) {
+                grunt.log.errorlns(err);
+            }
+
             db.collection(col, function(err, collection){
                 collection.remove({}, function(err, numRemovedDocs){
                     grunt.log.writeln(numRemovedDocs + " documents removed from " + col + " collection.");
@@ -88,9 +91,12 @@ module.exports = function (grunt) {
 
     function importData(col, obj, callback){
         client.connect(host, function(err, db) {
-            if (err) grunt.log.errorlns(err);
+            if (err) {
+                grunt.log.errorlns(err);
+            }
+
             db.collection(col, function(err, collection){
-                collection.insert(obj, function(err){
+                collection.insert(obj, function(){
                     db.close();
                     callback();
                 });
