41c45d33107644c63a822785696a1f3b2d82828f
Travis McHattie <travis.mchattie@thinksolid.com>
2014-02-16 14:26:19 -0800

    Fixed up test data.

--- tasks/fixtures/mongodb/db/test/content.js
+++ tasks/fixtures/mongodb/db/test/content.js
@@ -1,6 +1,29 @@
 module.exports = function(ObjectID) {
     'use strict';
 
-    var ids = require('./ids');
-    return [];
+    return [
+        {
+            __v: 0,
+            _id: ObjectID("53013a4aa937337206000011"),
+            author: {
+                _id: ObjectID("5246e73d56c02c0744000004")
+            },
+            changelog: [],
+            fields: {
+                workerurl: "http://localhost:9210",
+                repositorybranch: "live",
+                repositoryurl: "https://github.com/Solid-Interactive/bumblebee.git",
+                title: "Bumbleebee"
+            },
+            label: "Bumbleebee",
+            node: {
+                _id: ObjectID("5301352ba937337206000010")
+            },
+            slug: "bumbleebee",
+            status: "Live",
+            type: ObjectID("530133be230f86700600000e"),
+            validFrom: new Date("2014-02-16T22:23:06.695Z"),
+            validTo: new Date("2014-02-16T22:23:06.695Z")
+        }
+    ];
 };
