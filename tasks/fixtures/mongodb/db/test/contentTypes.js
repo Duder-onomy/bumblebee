e6ae7f8d628c1c14d2088e3bc5358114596d9b38
Travis McHattie <travis.mchattie@thinksolid.com>
2014-02-21 20:26:16 -0600

    Updated content type test data.

--- tasks/fixtures/mongodb/db/test/contentTypes.js
+++ tasks/fixtures/mongodb/db/test/contentTypes.js
@@ -3,9 +3,71 @@ module.exports = function(ObjectID) {
 
     return [
         {
-            __v: 3,
+            __v: 7,
             _id: ObjectID("530133be230f86700600000e"),
             description: "Content type that describes a bumblebee client worker bee.",
+            fields: [
+                {
+                    useAsLabel: true,
+                    _id: "identifier",
+                    validation: false,
+                    type: "textbox",
+                    options: false,
+                    min: 1,
+                    max: 1,
+                    label: "Identifier",
+                    required: true
+                },
+                {
+                    useAsLabel: false,
+                    _id: "description",
+                    validation: false,
+                    type: "textarea",
+                    options: false,
+                    min: 1,
+                    max: 1,
+                    label: "Description",
+                    required: true
+                },
+                {
+                    useAsLabel: false,
+                    _id: "url",
+                    validation: false,
+                    type: "textbox",
+                    options: false,
+                    min: 1,
+                    max: 1,
+                    label: "Url",
+                    required: true
+                },
+                {
+                    useAsLabel: false,
+                    _id: "repositories",
+                    validation: false,
+                    type: "embeddedtype",
+                    options: "5307faca230f86700600000f",
+                    min: 1,
+                    max: 975,
+                    label: "Repositories",
+                    required: false
+                },
+                {
+                    useAsLabel: false,
+                    _id: "logs",
+                    validation: false,
+                    type: "readonly",
+                    options: false,
+                    min: 1,
+                    max: 1000000000000,
+                    label: "Logs",
+                    required: false
+                }
+            ],
+            label: "Worker Bee"
+        },
+        {
+            __v: 5,
+            _id: ObjectID("5307faca230f86700600000f"),
             fields: [
                 {
                     useAsLabel: true,
@@ -31,28 +93,28 @@ module.exports = function(ObjectID) {
                 },
                 {
                     useAsLabel: false,
-                    _id: "repositorybranch",
+                    _id: "branch",
                     validation: false,
                     type: "textbox",
                     options: false,
                     min: 1,
                     max: 1,
-                    label: "Repository Branch",
+                    label: "Branch",
                     required: true
                 },
                 {
                     useAsLabel: false,
-                    _id: "workerurl",
+                    _id: "deploymentdestination",
                     validation: false,
                     type: "textbox",
                     options: false,
                     min: 1,
                     max: 1,
-                    label: "Worker Url",
-                    required: true
+                    label: "Deployment Destination",
+                    required: false
                 }
             ],
-            label: "Worker Bee"
+            label: "Repos"
         }
     ];
 };
